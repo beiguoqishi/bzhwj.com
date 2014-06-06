b734b0782b49776cf30f89b29d115e520ec3feb3
beiguoqishi <lpt2008303483@yahoo.cn>
2014-06-06 17:57:52 +0800

    add index_luobo sql.

--- data/member.sql
+++ data/member.sql
@@ -24,13 +24,15 @@ create table app_bzhwj_store_member(
   unique key store_user_idx(store_id,user_id)
 ) ENGINE=InnoDB CHARSET=utf8;
 
-create table app_bzhwj_store_member(
+create table app_bzhwj_index_luobo(
   id int unsigned not null auto_increment,
-  store_id int unsigned not null,
-  user_id int unsigned not null ,
+  title varchar(200),
+  url varchar(255),
+  imgurl varchar(1024),
+  ctg tinyint not null default 1,
   status tinyint not null default 1,
   create_at int unsigned,
   update_at int unsigned,
   primary key (id),
-  unique key store_user_idx(store_id,user_id)
+  unique key ctg_title_idx(ctg,title)
 ) ENGINE=InnoDB CHARSET=utf8;
